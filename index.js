fa06d04e677e8fb0f81a77b6f87e0aee3c09f007
Jimmy Esguerra <jimmy.esguerra@ofi.com>
2019-06-06 11:40:04 -0500

    cambios 3

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ console.log(math.divide(2,4));
 console.log(math.substract(2,4));
 console.log(math.add(2,4));
 
+//otra forma de llamar archivo
 console.log(greet.greet("eeeee"));
 
 console.log(hello.sayHello("carrr"));
