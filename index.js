d83c3b243bf382094904b92d9078cf37dd80fdf5
Jimmy Esguerra <jimmy.esguerra@ofi.com>
2019-06-06 11:55:01 -0500

    se soluciono el conflicto

--- index.js
+++ index.js
@@ -14,13 +14,7 @@ console.log(math.substract(2,4));
 console.log(math.add(2,4));
 
 //otra forma de llamar archivo
-<<<<<<< HEAD
 console.log(greet.greet("Karla"));
 //otro mas
 console.log(hello.sayHello("julia"));
 // purebas  mas pruebas
-=======
-console.log(greet.greet("camilo"));
-//otro mas
-console.log(hello.sayHello("jorge"));
->>>>>>> dev
